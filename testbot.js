24ab5992b3d42b81eaea89652c3aee4fe55a62ef
kokopun <engiball@hotmail.com>
2017-03-24 16:20:01 +0700

    H

--- testbot.js
+++ testbot.js
@@ -7,9 +7,10 @@ app.set("port", (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get("/bot", function(req, res) {
+app.get('/bot', function(req, res){
 res.send('ok');
-}
+
+});
 
 app.post("/callback", function(req, res) {
 	async.waterfall([function(callback) {
