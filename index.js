69fe61740b0885572ccb045db29559d7f8df04d3
kokopun <engiball@hotmail.com>
2017-03-24 14:58:08 +0700

    Port

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var http = require('http');
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<!DOCTYPE html>");
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
+});
 
-http.createServer(function(request, respone){
-  respone.writeHead(200, {'Content-type':'text/plan'});
-  response.write('Hello Node JS Server Response');
-  response.end( );
-
-}).listen(process.env.PORT);
+server.listen(8888);
+console.log("Server is listening");
